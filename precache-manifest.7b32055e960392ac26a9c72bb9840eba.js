9c65635d0e84349b1136eb16b87ca8a6fb4b6305
Sonmaster <congson1112@gmal.com>
2020-02-18 11:53:18 +0700

    Updates

---
+++ precache-manifest.7b32055e960392ac26a9c72bb9840eba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43615d29e4b1a550060cd3868d594a1",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "3c5d151c298546825c0e",
+    "url": "/redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a4488e1c680ce3b758c6",
+    "url": "/redux/static/css/main.37b6124f.chunk.css"
+  },
+  {
+    "revision": "3c5d151c298546825c0e",
+    "url": "/redux/static/js/2.f2c2834c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/redux/static/js/2.f2c2834c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4488e1c680ce3b758c6",
+    "url": "/redux/static/js/main.fb0ff335.chunk.js"
+  },
+  {
+    "revision": "1b58936011ed3060a473",
+    "url": "/redux/static/js/runtime-main.96801db3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
